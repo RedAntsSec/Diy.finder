cde84c94d0aa68a6b83c2a9cc5f621b071381e9c
Red Ants Team <63103476+RedAntsSec@users.noreply.github.com>
2020-08-28 13:07:19 +0430

    Update diy_finder.py

--- diy_finder.py
+++ diy_finder.py
@@ -1,9 +1,9 @@
 import requests
 import random
 import os
-import time
+from time import sleep
 from colorama import init
-
+from sys import argv
 
 
 def logo():
@@ -26,35 +26,55 @@ def logo():
 
 v1.0.1
     """)
-    print ('@RedAntsSec  (Github/Telegram)')
-    print ('\nCreated By j0hn for RED ANTS TEAM\n\nWE ARE IRANIAN HACKERS!\n')
-
+    print ('@RedAntsS3c  (Github/Telegram)')
+    print ('\nCreated By j0hn for RED ANTS TEAM\n\nWE ARE IRANIAN!\n')
 
 
 
 def finder():
-
-    di = open(dic, 'r')  #open directory file
+    try:
+        di = open(dic, 'r')  #open directory file
+    except:
+        print("\033[91m[-] File Not Found :( ")
+        exit()
     for d in di.readlines():
         
         try:
             d = d.replace('\n', '')
             url = target + d
-            r = requests.get(url)
+            
+            try:
+                if proxy != "":
+                    prox_conf = {protocol:proxy}
+                    url = "http://" + url
+                    r = requests.get(url, proxies=prox_conf)
+                else:
+                    url = "http://" + url
+                    r = requests.get(url) #send a request to url with http
+            except:
+                if proxy != "":
+                    prox_conf = {protocol:proxy}
+                    url = "https://" + url
+                    r = requests.get(url, proxies=prox_conf)
+                else:
+                    url = "https://" + url
+                    r = requests.get(url) #send a request to url with https
         except KeyboardInterrupt:
             print('\n\033[91mUser Using CTRL-C ...')
-            time.sleep(2)  #sleep 2 second 
+            sleep(2)  #sleep 2 second 
             print('\n\033[91mExiting ... ')
-            time.sleep(2)  #sleep 2 second 
+            sleep(2)  #sleep 2 second 
             exit()
         except:
             print ('\033[91m[-] Site Not Found [-]')
             exit()
         if r.status_code == 200:
             print ('\n\033[92m[+] Directory of Target Found : %s' % url)
-        
+            success = open("Success.txt","a")
+            success.write(url + "\n") #write success urls in file
+            success.close() #close the file
         else:
-            print ('\n\033[91m[*] Not Found : %s ' % url)
+            print ('\n\033[91m[i] Not Found : %s ' % url)
 
 
 
@@ -66,22 +86,45 @@ def finder():
 def main():
     global target
     global dic
+    global proxy
+    global protocol
     init()
     logo()
-
     try:
-        
-        target = str(input('Enter Target with http-> '))
-        dic    = str(input('Enter Dictionary File -> '))
+        try:
+            target = argv[1]
+            try:
+                dic = argv[2] 
+            except:
+                dic = "path.txt"
+            try:
+                proxy = argv[3]
+            except:
+                proxy = ""
+            try:
+                protocol = argv[4]
+            except:
+                protocol = ""
+        except:
+            
+            print ("\033[93m\nUsage: python "+argv[0]+" example.com path.txt proxy protocol_of_proxy")
+            exit()
+            if "http://" in target or "https://" in target:
+                target = target.replace("http://","")
+                target = target.replace("https://","")
+                if target[-1] == "/":
+                    target = target.replace("/","")
+                    print ("[ ~ ] Your Target: " + target)
+            
     except KeyboardInterrupt:
         print('\033[91mUser Using CTRL-C ...')
-        time.sleep(2)
+        sleep(2)
         print('\033[91mExiting ... ')
-        time.sleep(2)
+        sleep(2)
         exit()
     if dic == '':
         print ('\033[91mNo File Found\nExiting ...')
-        time.sleep(2)
+        sleep(2)
         exit()
     else:
         pass
@@ -89,14 +132,14 @@ def main():
     finder()
 while True:
     main()
-    i = str(input('\033[93m[~] Do You Want To Continue yes or no [y or n] -> '))
+    i = str(input('\033[93m\n[~] Do You Want To Continue? [Y/n] -> '))
     if i == 'y' or i == 'Y':
         main()
     if i == 'n' or i == 'N':
         print ('\033[31mExiting...')
-        time.sleep(2)
+        sleep(2)
         exit()
     else:
         print ('\033[31mExiting...')
-        time.sleep(2)
+        sleep(2)
         exit()
